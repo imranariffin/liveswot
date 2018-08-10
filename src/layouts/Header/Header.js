a4dbfcdcf16f37eb8cd0d933aabd44eb9097bc4d
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-10 01:02:35 -0400

    WIP Header

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -5,32 +5,35 @@ import styles from './styles.scss';
 
 const Header = ({logout, username, email, profileImg, isLoggedIn}) => {
   return (
-    <nav className={`${styles.root}`}>
-      <div className={`${styles["nav-wrapper"]}`}>
-        <div className={`${styles["row-no-margin"]}`}>
-          <div></div>
-          <div className={`${styles["nav-main"]}`}>
-            <Link to='/'>{localizedText().title}</Link>
-            <ul id='nav-mobile'>
-              {!isLoggedIn && <li><Link to='/login'>Login</Link></li>}
-              {isLoggedIn && <li><Link to='' onClick={() => logout()}>Logout</Link></li>}
-              {isLoggedIn &&
-              <li className={`${styles["nav-bar-list"]}`}>
-                <Link to='/profile'>
-                  <div className={`${styles["profile-img-wrapper"]}`}>
-                    <img
-                      alt={username}
-                      className={`${styles["profile-img"]}`}
-                      src={profileImg}
-                    />
-                  </div>
-                </Link></li>}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <div className={`${styles.root}`}>
+      <div className={`${styles.brand}`}>LiveSwot</div>
+      <div className={`${styles.items}`}></div>
+      <div className={`${styles.profile}`}></div>
+    </div>
   );
 };
+
+{/*<div className={`${styles["row-no-margin"]}`}>*/}
+  {/*<div></div>*/}
+  {/*<div className={`${styles["nav-main"]}`}>*/}
+    {/*<a href='/'>{localizedText().title}</a>*/}
+    {/*<ul id='nav-mobile'>*/}
+      {/*{!isLoggedIn && <li><a href='/login'>Login</a></li>}*/}
+      {/*{isLoggedIn && <li><a href='' onClick={() => logout()}>Logout</a></li>}*/}
+      {/*{isLoggedIn &&*/}
+      {/*<li className={`${styles["nav-bar-list"]}`}>*/}
+        {/*<a href='/profile'>*/}
+          {/*<div className={`${styles["profile-img-wrapper"]}`}>*/}
+            {/*<img*/}
+              {/*alt={username}*/}
+              {/*className={`${styles["profile-img"]}`}*/}
+              {/*src={profileImg}*/}
+            {/*/>*/}
+          {/*</div>*/}
+        {/*</a></li>}*/}
+    {/*</ul>*/}
+  {/*</div>*/}
+  {/*<div></div>*/}
+{/*</div>*/}
 
 export default Header;
