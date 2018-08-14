350d254e7c2802c3e1329dfa8a0fb894f71a4c4c
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-14 19:39:36 -0400

    Header: cleanup

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import { localizedText } from '../../utils/index';
 import styles from './styles.scss';
 
@@ -17,28 +16,5 @@ const Header = ({logout, username, email, profileImg, isLoggedIn}) => {
     </div>
   );
 };
-
-{/*<div className={`${styles["row-no-margin"]}`}>*/}
-  {/*<div></div>*/}
-  {/*<div className={`${styles["nav-main"]}`}>*/}
-    {/*<a href='/'>{localizedText().title}</a>*/}
-    {/*<ul id='nav-mobile'>*/}
-      {/*{!isLoggedIn && <li><a href='/login'>Login</a></li>}*/}
-      {/*{isLoggedIn && <li><a href='' onClick={() => logout()}>Logout</a></li>}*/}
-      {/*{isLoggedIn &&*/}
-      {/*<li className={`${styles["nav-bar-list"]}`}>*/}
-        {/*<a href='/profile'>*/}
-          {/*<div className={`${styles["profile-img-wrapper"]}`}>*/}
-            {/*<img*/}
-              {/*alt={username}*/}
-              {/*className={`${styles["profile-img"]}`}*/}
-              {/*src={profileImg}*/}
-            {/*/>*/}
-          {/*</div>*/}
-        {/*</a></li>}*/}
-    {/*</ul>*/}
-  {/*</div>*/}
-  {/*<div></div>*/}
-{/*</div>*/}
 
 export default Header;
