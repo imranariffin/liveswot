8acd177cfb15464f9a345ab74eb5780061f88ba0
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-15 22:37:57 -0400

    WIP: SwotList: hide/show kebab, grey on hover

--- src/components/SwotList/SwotList.js
+++ src/components/SwotList/SwotList.js
@@ -1,25 +1,49 @@
 import React from 'react';
 import styles from './SwotList.scss';
 import Kebab from '../Kebab/';
+import {Link} from 'react-router-dom';
 
-const SwotList = (props) => {
-  // const swotImg = '';
-  const {swotId, title, description, swotDateCreated, creatorUserName} = props;
+class SwotList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: false};
 
-  return (
-    <div className={styles.root}>
-      <div className={styles.row}>
-        <div className={styles.creator}></div>
-        <div className={styles.content}>
-          <div className={styles.title}>{title}</div>
-          <div className={styles.description}>{description}</div>
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+
+  onMouseEnter() {
+    this.setState({active: true});
+  }
+
+  onMouseLeave() {
+    this.setState({active: false});
+  }
+
+  render() {
+    // const swotImg = '';
+    const {swotId, title, description, swotDateCreated, creatorUserName} = this.props;
+
+    return (
+      <Link
+        to={`swots/${swotId}`}
+        className={styles.root}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        <div className={styles.row}>
+          <div className={styles.creator}></div>
+          <div className={styles.content}>
+            <div className={styles.title}>{title}</div>
+            <div className={styles.description}>{description}</div>
+          </div>
+          <div className={`${styles.actions} ${this.state.active ? styles.active : ''}`}>
+            <Kebab/>
+          </div>
         </div>
-        <div className={styles.actions}>
-          <Kebab/>
-        </div>
-      </div>
-    </div>
-  );
-};
+      </Link>
+    );
+  }
+}
 
 export default SwotList;
