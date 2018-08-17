c8619afb9c33c2284a9c18488e9dc561fd7d7289
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-17 18:57:13 -0400

    brand routes to root

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { localizedText } from '../../utils/index';
 import styles from './Header.scss';
+import {Link} from 'react-router-dom';
 
 const Header = ({logout, username, email, profileImg, isLoggedIn}) => {
   return (
     <div className={`${styles.root}`}>
       <div className={styles.container}>
-        <div className={`${styles.brand}`}>LiveSWOT</div>
+        <Link className={`${styles.brand}`} to={`/`}>LiveSWOT</Link>
         <div className={`${styles["items-container"]}`}>
           <div className={`${styles.item} ${styles["left-end"]}`}>Item 1</div>
           <div className={styles.item}>Item 2</div>
