72e2ee9ce16d8094c61d520b5875d604a07894f3
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-18 13:28:54 -0400

    Login: card container

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -3,6 +3,7 @@ import styles from './Login.scss';
 import RequestButton from '../../components/RequestButton';
 import RequestError from '../../components/RequestError/RequestError';
 import {Link} from 'react-router-dom';
+import Card from '../../components/Card/Card';
 
 class Login extends React.Component {
 
@@ -20,42 +21,32 @@ class Login extends React.Component {
     const {user, disabled} = this.props;
 
     return (
-      <div className={`row`}>
-        <div className={`col s1 m3 l3`}></div>
-        <div className={`col s10 m6 l6`}>
-          <div className={`card ${styles.card}`}>
-            <div className={`row`}>
-              <div className={`col s1 m1 l1`}></div>
-              <div className={`col s10 m10 l10`}>
-                <h4 className={styles.title}>Login to LiveSWOT</h4>
-                <form onSubmit={this.login} >
-                  <input
-                    type='email'
-                    ref='email'
-                    placeholder={`Email`}
-                  />
-                  <input
-                    type='password'
-                    ref='password'
-                    placeholder={`Password`}
-                  />
-                  <input type='submit' className={styles.hidden}/>
-                  <RequestButton
-                    text={`login`}
-                    disabled={disabled}
-                    requestedItem={user}
-                    onClick={this.login}
-                  />
-                </form>
-                <p>Not signed up yet? Signup <Link to={`signup`}>here</Link></p>
-                <RequestError errors={user.errors}/>
-              </div>
-              <div className={`col s1 m1 l1`}></div>
-            </div>
-          </div>
+      <Card>
+        <div className={styles['form-container']}>
+          <h4 className={styles.title}>Login to LiveSWOT</h4>
+          <form className={styles.form} onSubmit={this.login}>
+            <input
+              type='email'
+              ref='email'
+              placeholder={`Email`}
+            />
+            <input
+              type='password'
+              ref='password'
+              placeholder={`Password`}
+            />
+            <input type='submit' className={styles.hidden}/>
+            <RequestButton
+              text={`login`}
+              disabled={disabled}
+              requestedItem={user}
+              onClick={this.login}
+            />
+          </form>
+          <p>Not signed up yet? Signup <Link to={`signup`}>here</Link></p>
+          <RequestError errors={user.errors}/>
         </div>
-        <div className={`col s1 m3 l3`}></div>
-      </div>
+      </Card>
     );
   }
 }
