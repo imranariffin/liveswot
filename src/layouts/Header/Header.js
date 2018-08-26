woakes070048/liveswot-ui

Header: remove all css

src/layouts/Header/Header.js
@@ -6,34 +6,32 @@ import styles from './styles.scss';
 
 const Header = ({logout, username, email, profileImg, isLoggedIn}) => {
   return (
-    <div className={`navbar-fixed ${styles["nav-fixed"]}`}>
-      <nav className={`${styles.nav}`}>
-        <div className={`nav-wrapper ${styles["nav-wrapper"]}`}>
-          <div className={`row ${styles["row-no-margin"]}`}>
-            <div className={`col l1`}></div>
-            <div className={`col l10 ${styles["nav-main"]}`}>
-              <a href='/' className='brand-logo'>{localizedText().title}</a>
-              <ul id='nav-mobile' className='right hide-on-med-and-down'>
-                {!isLoggedIn && <li><a href='/login'>Login</a></li>}
-                {isLoggedIn && <li><a href='' onClick={() => logout()}>Logout</a></li>}
-                {isLoggedIn &&
-                <li className={`${styles["nav-bar-list"]}`}>
-                  <a href='/profile'>
-                    <div className={`right ${styles["profile-img-wrapper"]}`}>
-                      <img
-                        alt={username}
-                        className={`circle responsive-img ${styles["profile-img"]}`}
-                        src={profileImg}
-                      />
-                    </div>
-                  </a></li>}
-              </ul>
-            </div>
-            <div className={`col l1`}></div>
+    <nav className={`${styles.root}`}>
+      <div className={`${styles["nav-wrapper"]}`}>
+        <div className={`${styles["row-no-margin"]}`}>
+          <div></div>
+          <div className={`${styles["nav-main"]}`}>
+            <a href='/'>{localizedText().title}</a>
+            <ul id='nav-mobile'>
+              {!isLoggedIn && <li><a href='/login'>Login</a></li>}
+              {isLoggedIn && <li><a href='' onClick={() => logout()}>Logout</a></li>}
+              {isLoggedIn &&
+              <li className={`${styles["nav-bar-list"]}`}>
+                <a href='/profile'>
+                  <div className={`${styles["profile-img-wrapper"]}`}>
+                    <img
+                      alt={username}
+                      className={`${styles["profile-img"]}`}
+                      src={profileImg}
+                    />
+                  </div>
+                </a></li>}
+            </ul>
           </div>
+          <div></div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 

imranariffin/liveswot

src/layouts/Header/Header.js
@@ -5,34 +5,31 @@ import styles from './styles.scss';
 
 const Header = ({logout, username, email, profileImg, isLoggedIn}) => {
   return (
-    <div className={`navbar-fixed ${styles["nav-fixed"]}`}>
-      <nav className={`${styles.nav}`}>
-        <div className={`nav-wrapper ${styles["nav-wrapper"]}`}>
-          <div className={`row ${styles["row-no-margin"]}`}>
-            <div className={`col l1`}></div>
-            <div className={`col l10 ${styles["nav-main"]}`}>
-              <Link to='/' className='brand-logo'>{localizedText().title}</Link>
-              <ul id='nav-mobile' className='right hide-on-med-and-down'>
-                {!isLoggedIn && <li><Link to='/login'>Login</Link></li>}
-                {isLoggedIn && <li><Link to='' onClick={() => logout()}>Logout</Link></li>}
-                {isLoggedIn &&
-                <li className={`${styles["nav-bar-list"]}`}>
-                  <Link to='/profile'>
-                    <div className={`right ${styles["profile-img-wrapper"]}`}>
-                      <img
-                        alt={username}
-                        className={`circle responsive-img ${styles["profile-img"]}`}
-                        src={profileImg}
-                      />
-                    </div>
-                  </Link></li>}
-              </ul>
-            </div>
-            <div className={`col l1`}></div>
+    <nav className={`${styles.root}`}>
+      <div className={`${styles["nav-wrapper"]}`}>
+        <div className={`${styles["row-no-margin"]}`}>
+          <div></div>
+          <div className={`${styles["nav-main"]}`}>
+            <Link to='/'>{localizedText().title}</Link>
+            <ul id='nav-mobile'>
+              {!isLoggedIn && <li><Link to='/login'>Login</Link></li>}
+              {isLoggedIn && <li><Link to='' onClick={() => logout()}>Logout</Link></li>}
+              {isLoggedIn &&
+              <li className={`${styles["nav-bar-list"]}`}>
+                <Link to='/profile'>
+                  <div className={`${styles["profile-img-wrapper"]}`}>
+                    <img
+                      alt={username}
+                      className={`${styles["profile-img"]}`}
+                      src={profileImg}
+                    />
+                  </div>
+                </Link></li>}
+            </ul>
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 